d77eafb7c12d8eaa356171ca7cdb477194b3b41b
Alexander Kudlai <akudlay@hotmail.com>
2018-12-16 21:00:23 +0200

    Lesson 2 Task 2 implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyTableComponent } from './my-table/my-table.component';
-import { MyTableHostComponent } from './my-table/my-table-host.component'
+import { MyTableHostComponent } from './my-table/my-table-host.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { MyTableHostComponent } from './my-table/my-table-host.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
-import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-my-table',
@@ -10,25 +9,38 @@ export class MyTableComponent implements OnInit {
 
   @Input() rows: number = 0;
 
-  public Products: any;
+  public Products: any = [
+    { id: 1, name: 'product 1', price: 100, category: 'Category 1' },
+    { id: 2, name: 'product 2', price: 200, category: 'Category 2' },
+    { id: 3, name: 'product 3', price: 300, category: 'Category 3' },
+    { id: 4, name: 'product 4', price: 400, category: 'Category 1' },
+    { id: 5, name: 'product 5', price: 500, category: 'Category 2' },
+    { id: 6, name: 'product 6', price: 600, category: 'Category 3' },
+    { id: 7, name: 'product 7', price: 700, category: 'Category 1' },
+    { id: 8, name: 'product 8', price: 800, category: 'Category 2' },
+    { id: 9, name: 'product 9', price: 900, category: 'Category 3' },
+    { id: 10, name: 'product 10', price: 1000, category: 'Category 1' }
+  ];
+  public options: Array<string> = ['Category 1', 'Category 2', 'Category 3'];
+  public selected: string;
+  public selectedData: any;
 
-  constructor() { }
+  constructor() { this.selectedData = this.Products; }
 
   ngOnInit() {
-    this.Products =
-                    [{ id: 1, name : 'product 1', price : 100 },
-                     { id: 2, name : 'product 2', price : 200 },
-                     { id: 3, name : 'product 3', price : 300 },
-                     { id: 4, name : 'product 4', price : 400 },
-                     { id: 5, name : 'product 5', price : 500 },
-                     { id: 6, name : 'product 6', price : 600 },
-                     { id: 7, name : 'product 7', price : 700 },
-                     { id: 8, name : 'product 8', price : 800 },
-                     { id: 9, name : 'product 9', price : 900 },
-                     { id: 10, name : 'product 10', price : 1000 }];
+  }
+
+  onSelect(val) {
+    if(val != 'all'){
+      this.selectedData = this.Products.filter(p => p.category == val);
+    }
+    else {
+      this.selectedData = this.Products;
+    }
+
   }
 
   deleteRow(index) {
-      this.Products.splice(index, 1);
+    this.Products.splice(index, 1);
   }
 }
